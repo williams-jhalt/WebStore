1eac431cc4d1335d7e32454f9929814be112d4b0
johnh <johnh@ofc-dev-1>
2015-02-23 14:26:34 -0500

    Updated Dockerfile packages

--- app/config/eb_parameters.php
+++ app/config/eb_parameters.php
@@ -1,8 +1,8 @@
 <?php
 
 $container->setParameter('database_driver', 'pdo_mysql');
-$container->setParameter('database_host', $_ENV['RDS_HOSTNAME']);
-$container->setParameter('database_port', $_ENV['RDS_PORT']);
-$container->setParameter('database_name', $_ENV['RDS_DB_NAME']);
-$container->setParameter('database_user', $_ENV['RDS_USERNAME']);
-$container->setParameter('database_password', $_ENV['RDS_PASSWORD']);
+$container->setParameter('database_host', getenv('RDS_HOSTNAME'));
+$container->setParameter('database_port', getenv('RDS_PORT'));
+$container->setParameter('database_name', getenv('RDS_DB_NAME'));
+$container->setParameter('database_user', getenv('RDS_USERNAME'));
+$container->setParameter('database_password', getenv('RDS_PASSWORD'));
