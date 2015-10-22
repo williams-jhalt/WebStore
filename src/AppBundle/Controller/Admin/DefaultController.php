bb6f92ebe46c4fd5de180b2c0be1557dffcaeaa7
johnh <johnh@DESKTOP-B7JMS42.hq.williamstradingco.com>
2015-10-22 10:06:25 -0400

    erp data revamp

--- src/AppBundle/Controller/Admin/DefaultController.php
+++ src/AppBundle/Controller/Admin/DefaultController.php
@@ -21,13 +21,20 @@ class DefaultController extends Controller {
         $numberOfManufacturers = $em->createQuery('SELECT COUNT(p) FROM AppBundle:Manufacturer p')->getSingleScalarResult();
         $numberOfUsers = $em->createQuery('SELECT COUNT(p) FROM AppBundle:User p')->getSingleScalarResult();
         $numberOfProductTypes = $em->createQuery('SELECT COUNT(p) FROM AppBundle:ProductType p')->getSingleScalarResult();
+        $numberOfOpenOrders = $em->createQuery('SELECT COUNT(o) FROM AppBundle:SalesOrder o WHERE open = true')->getSingleScalarResult();
+        $ordersPerDay = $em->createQuery('SELECT COUNT(*) FROM AppBundle:SalesOrder o WHERE orderDate > DATE_SUB(orderDate, 5, DAY) GROUP BY orderDate')->getScalarResult();
+        
+        $avgOrdersPerDay = array_sum($ordersPerDay) / sizeof($ordersPerDay);
+        
 
         return array(
             'number_of_products' => $numberOfProducts,
             'number_of_categories' => $numberOfCategories,
             'number_of_manufacturers' => $numberOfManufacturers,
             'number_of_product_types' => $numberOfProductTypes,
-            'number_of_users' => $numberOfUsers
+            'number_of_users' => $numberOfUsers,
+            'number_of_open_orders' => $numberOfOpenOrders,
+            'avg_orders_per_day' => $avgOrdersPerDay                
         );
     }
 
